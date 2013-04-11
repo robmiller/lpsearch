80b9894c9f7880f173bf4398567b1462c07a01fb
Rob Miller <rob@bigfish.co.uk>
2013-04-11 22:22:27 +0100

    Initial revision
    Outputs the tasks, with URLs, that match your query.

---
+++ lib/lpsearch.rb
@@ -0,0 +1,60 @@
+require 'liquidplanner'
+require 'highline/import'
+require 'json'
+require 'pp'
+
+class LPSearch
+	def initialize
+		@creds = {}
+		@creds_file = File.expand_path "~/.lprc"
+
+		if File.readable? @creds_file
+			get_credentials
+		else
+			ask_credentials
+		end
+
+		@lp = LiquidPlanner::Base.new({:email => @creds["email"], :password => @creds["pass"]})
+		@workspace = @lp.workspaces @creds["space"]
+	end
+
+	def get_credentials
+		json = IO.read(@creds_file)
+
+		if json.length < 2
+			ask_credentials
+		end
+
+		begin
+			@creds = JSON.parse(json)
+		rescue
+		ensure
+			ask_credentials unless @creds.length
+		end
+	end
+
+	def ask_credentials
+		unless stdin.tty?
+			puts "I don't have a TTY. Run me from the command line first so I can get your LP credentials."
+			exit
+		end
+
+		email = ask("Email: ") { |q| q.validate = /@/ }
+		pass  = ask("Password: ") { |q| q.echo = "x" }
+		space = ask("LP space ID: ") { |q| q.validate = /^[0-9]+$/ }
+
+		@creds = { "email" => email, "pass" => pass, "space" => space }
+
+		File.open(@creds_file, "w") { |f| f.write(@creds.to_json) }
+	end
+
+	def search(query)
+		tasks = @workspace.tasks(:all, :limit => 5, :filter => "name contains '#{query}'")
+		tasks.each do |task|
+			t = { :name => task.name, :url => "http://app.liquidplanner.com/space/#{@creds["space"]}/projects/show/#{task.id}" }
+
+			yield t
+		end
+	end
+end
+
